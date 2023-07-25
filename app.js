78716646104982efe47ff6137740e41aaab492f6
Elu1919 <qw29149974@gmail.com>
2023-07-26 02:30:47 +0800

    chore: heroku init

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const methodOverride = require('method-override')
 
 const express = require('express')
 const app = express()
-const port = 3000
+const PORT = process.env.PORT || 3000
 
 
 // setting express-handlebars
@@ -50,6 +50,6 @@ app.use(methodOverride('_method'))
 app.use(routes)
 
 
-app.listen(port, () => {
-  console.log(`Express is running on http://localhost:${port}`)
+app.listen(PORT, () => {
+  console.log(`Express is running on http://localhost:${PORT}`)
 })
